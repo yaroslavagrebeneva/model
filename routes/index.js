c8353ceb70ed6f670add276f2c6b30c618081695
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-17 16:23:20 +0300

    Пункт_4_4

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 router.get('/bellahadid', function(req, res, next) {
   res.render('model', {
   title: "Bella Hadid",
-  picture: "images/bella.png",
+  picture: "images/bella.jpg",
   desc: "Американская супермодель палестино-голландского происхождения. Является послом и постоянным лицом модных домов Dior и Bulgari. Одна из самых известных и высокооплачиваемых моделей мира.  "
   });
   });
@@ -20,7 +20,7 @@ router.get('/bellahadid', function(req, res, next) {
 router.get('/irinashayk', function(req, res, next) {
   res.render('model', {
   title: "Irina Shayk",
-  picture: "images/irina.png",
+  picture: "images/irina.jpg",
   desc: "Российская супермодель и актриса. Она является первой российской моделью, появившейся на обложке Sports Illustrated Swimsuit Issue. Сайт Models.com назвал её иконой модной индустрии.  "
   });
   });
@@ -29,7 +29,7 @@ router.get('/irinashayk', function(req, res, next) {
 router.get('/naomicampbell', function(req, res, next) {
   res.render('model', {
   title: "Naomi Campbell",
-  picture: "images/naomi.png",
+  picture: "images/naomi.jpg",
   desc: "Британская супермодель, актриса и певица афроямайского и китайско-ямайского происхождения. Работает на подиумах с 15 лет. Стала первой темнокожей девушкой, появившейся на обложках французского и английского изданий журнала «Vogue» и журнала «Time». "
   });
   });
