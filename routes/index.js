0997e1acfb15578228906a17b399b791ca91bfca
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-17 14:21:05 +0300

    Пункт_4_2

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* Страница Беллы Хадид*/
+router.get('/bellahadid', function(req, res, next)
+ {
+  res.send("<h1>Страница Беллы Хадид</h1>")
+  });
+
+    /* Страница Ирины Шейк*/
+router.get('/irinashayk', function(req, res, next) 
+{
+ res.send("<h1>Страница Ирины Шейк</h1>")
+ });
+
+/* Страница Наоми Кэмпбелл*/
+ router.get('/naomicampbell', function(req, res, next)
+{
+ res.send("<h1>Наоми Кэмпбелл</h1>")
+ });
+
+module.exports = router;
