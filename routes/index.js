c00f3429d21558c90f9fd7caa1ef0fe5498967e4
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-10-28 13:51:47 +0300

    Пункт_4_3

--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,32 @@ router.get('/', function(req, res, next) {
 
 /* Страница Беллы Хадид*/
 router.get('/bellahadid', function(req, res, next) {
-  res.send("<h1>Страница Беллы Хадид</h1>")
+  res.render('model', {
+  title: "Bella Hadid",
+  picture: "images/bella.jpg",
+  desc: "Американская супермодель палестино-голландского происхождения. Является послом и постоянным лицом модных домов Dior и Bulgari. Одна из самых известных и высокооплачиваемых моделей мира.  "
   });
+  });
+  
 
   /* Страница Ирины Шейк*/
 router.get('/irinashayk', function(req, res, next) {
-  res.send("<h1>Страница Ирины Шейк</h1>")
+  res.render('model', {
+  title: "Irina Shayk",
+  picture: "images/irina.jpg",
+  desc: "Российская супермодель и актриса. Она является первой российской моделью, появившейся на обложке Sports Illustrated Swimsuit Issue. Сайт Models.com назвал её иконой модной индустрии.  "
+  });
+  });
+  
+  /* Страница Наоми Кэмпбелл*/
+router.get('/naomicampbell', function(req, res, next) {
+  res.render('model', {
+  title: "Naomi Campbell",
+  picture: "images/naomi.jpg",
+  desc: "Британская супермодель, актриса и певица афроямайского и китайско-ямайского происхождения. Работает на подиумах с 15 лет. Стала первой темнокожей девушкой, появившейся на обложках французского и английского изданий журнала «Vogue» и журнала «Time». "
+  });
   });
 
- /* Страница Наоми Кэмпбелл*/
-router.get('/naomicampbell', function(req, res, next) {
-  res.send("<h1>Страница Наоми Кэмпбелл</h1>")
-  });
 
 
 module.exports = router;
