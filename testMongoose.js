281ff6bd1b8f63ed4d2776dc7ae6a6a6cfa56ad8
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-11-13 15:21:04 +0300

    yasya_model_7_2

--- testMongoose.js
+++ testMongoose.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-const Model = mongoose.model('Model', { name: String });
-const modeling = new Model({ name: 'Monica Belucci' });
-modeling.save().then(() => console.log('Smile!'));
+
+var schema = mongoose.Schema({ name: String })
+schema.methods.smile = function(){
+    console.log(this.name + " say smile")
+    }
+    const Model = mongoose.model('Model', schema);
+    const modeling = new Model({ name: 'Monica Belucci' });
+    modeling.save().then(() => modeling.smile());
