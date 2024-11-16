92afa1e756cfc097086f90f3e6964ad074edb135
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-11-16 14:20:54 +0300

    yasya_model_8_2

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var models = require('./routes/models');
 
-
 var app = express();
 
 // view engine setup
