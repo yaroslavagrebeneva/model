91dd2c006cfb2406551f403d91c6f73179f9953d
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-11-16 14:17:01 +0300

    yasya_model_8_2

--- routes/models.js
+++ routes/models.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с models');
 });
+
 /* Страница models */
 router.get("/:nick", function(req, res, next) {
   res.send(req.params.nick);
