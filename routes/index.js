37e81ea5b553d03dd8eccd3568a69e10639ebb7c
anarhisssttt <grebeneva.y.i.23@gmail.com>
2024-11-18 13:07:05 +0300

    yasya_model_10_1

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express',  counter:req.session.counter , picture: "images/welcome.jpg"});
 });
 
-
+/* GET login/registration page. */
+router.get('/logreg', function(req, res, next) {
+  res.render('logreg',{title: 'Вход'});
+  });
+  
 module.exports = router;
